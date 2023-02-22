3ad1a8bf9e195daf26b7072b5c5a962857341aa2
DoCtEuR3805 | FRENCH-QC <35934387+dan3805@users.noreply.github.com>
2023-02-21 23:08:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ 
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-  ]
-});
+client = new Discord.Client({intents: 32767})
 const express = require('express');
 const bodyParser = require('body-parser');
 
