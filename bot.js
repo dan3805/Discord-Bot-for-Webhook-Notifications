f5e064c5e43b961135bdb8c4fa86b20c98599859
DoCtEuR3805 | FRENCH-QC <35934387+dan3805@users.noreply.github.com>
2023-02-22 00:48:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [32767] });
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const SECRET = process.env.SECRET || 'default_secret_value';
+
+// Set the message for the persistent message footer
+const MESSAGE = process.env.MESSAGE || 'Default persistent message';
 
 const app = express();
 const persistentMessageChannelID = process.env.CHANNEL_ID || 'Channel_ID_HERE';
 const TOKEN = process.env.TOKEN || 'DISCORD_BOT_TOKEN_HERE';
-console.log(`Token: ${TOKEN}`);
 let persistentMessage = null;
 
 app.use(bodyParser.json());
@@ -19,69 +23,91 @@ client.on('ready', async () => {
 
   // Check if a persistent message has already been sent in the channel
   const messages = await channel.messages.fetch();
-  persistentMessage = messages.find(m => m.author.id === client.user.id && m.embeds[0].footer.text === 'Persistent message');
+  persistentMessage = messages.find(m => m.author.id === client.user.id && m.embeds[0].footer.text === MESSAGE);
 
-  // If a persistent message has already been sent, we retrieve its ID
+  // If a persistent message has already been sent, retrieve its ID
   if (persistentMessage) {
-    console.log(`ID of the persistent message retrieved: ${persistentMessage.id}`);
-  } else {
+    console.log(`ID of existing persistent message: ${persistentMessage.id}`);
+  }
+  // Otherwise, send a new persistent message and retrieve its ID
+  else {
     persistentMessage = await channel.send({
-      embeds: [{
+      embed: {
         title: 'Persistent message',
         description: 'This is a persistent message that will be updated',
         color: 0x00ff00,
         footer: {
-          text: 'Persistent message'
+          text: 'Default persistent message'
         }
-      }]
+      }
     });
-    console.log(`ID of the persistent message created: ${persistentMessage.id}`);
+    console.log(`ID of new persistent message: ${persistentMessage.id}`);
   }
+
+  // Ensure that all required variables have been defined before attempting to use them
+  if (!MESSAGE) {
+    console.error('Please define the MESSAGE environment variable');
+    process.exit(1);
+  }
+
+  if (!persistentMessage) {
+    console.error('Persistent message not found');
+    process.exit(1);
+  }
+
+  const embed = persistentMessage.embeds[0];
+
+  persistentMessage.edit({
+    embed: embed
+  }).then(() => {
+    console.log('Persistent message updated successfully');
+  }).catch((error) => {
+    console.error(`Error updating persistent message: ${error}`);
+  });
 });
 
 // Receive notifications from the Overseerr API
-app.post('/notification-endpoint', (req, res) => {
+app.post('/notification-endpoint/:secret', (req, res) => {
+  const secret = req.params.secret;
+
+  // Check if the secret parameter matches the expected value
+  if (secret !== process.env.SECRET) {
+    console.log(`Invalid secret parameter: ${secret}`);
+    res.status(401).end();
+    return;
+  }
+
   const notification = req.body;
   console.log(`New notification received: ${JSON.stringify(notification)}`);
 
-  // If the persistent message has not been found, return a 404 error
-  if (!persistentMessage) {
-    console.log('Persistent message not found');
-    return res.status(404).end();
-  }
-
-  const embed = persistentMessage.embeds.length > 0 ? { ...persistentMessage.embeds[0] } : {};
-
-  if (embed.footer && embed.footer.text === 'Persistent message') {
-    // Update the embed message with the new values
-    if (notification.update_embed) {
-      for (const [key, value] of Object.entries(notification.update_embed)) {
-        if (key in embed) {
-          embed[key] = value;
-        } else if (key === 'fields') {
-          // Update a field based on its content
-          value.forEach(newField => {
-            const oldField = embed.fields.find(field => field.name.includes(newField.name));
-            if (oldField) {
-              oldField.name = newField.name;
-              oldField.value = newField.value;
-            }
-          });
+  const embed = { ...persistentMessage.embeds[0] };
+if (notification.update_embed) {
+  for (const [key, value] of Object.entries(notification.update_embed)) {
+    if (key in embed) {
+      embed[key] = value;
+    } else if (key === 'fields') {
+      value.forEach(newField => {
+        const oldField = embed.fields.find(field => field.name.includes(newField.name));
+        if (oldField) {
+          oldField.name = newField.name;
+          oldField.value = newField.value;
         }
-      }
+      });
     }
+  }
+}
 
-    // If the "update_field" field is present, we update the specified field in the embed message
-    if (notification.update_field) {
-      const { name, value } = notification.update_field;
-      const field = embed.fields.find(field => field.name.includes(name));
-      if (field) {
-        field.name = name;
-        field.value = value;
-      }
-    }
+// If the "update_field" field is present, update the specified field in the message embed
+if (notification.update_field) {
+  const { name, value } = notification.update_field;
+  const field = embed.fields.find(field => field.name.includes(name));
+  if (field) {
+    field.name = name;
+    field.value = value;
+  }
+}
 
-// If the "delete_field" field is present, we remove the specified field from the embed message
+// If the "delete_field" field is present, remove the specified field from the message embed
 if (notification.delete_field) {
   const index = embed.fields.findIndex(field => field.name === notification.delete_field);
   if (index !== -1) {
@@ -89,28 +115,26 @@ if (notification.delete_field) {
   }
 }
 
-}
-if (!embed || !embed.title) {
-  console.log('Invalid embed structure');
-  return res.status(400).end();
-}
-
-persistentMessage.edit({
-  embeds: [embed]
-})
-  .then(() => {
-    res.status(200).end();
-  })
-  .catch(error => {
-    console.error('Error editing message:', error);
-    res.status(500).end();
+// Check if persistent message is not null
+if (persistentMessage) {
+  persistentMessage.edit({
+    embed: embed
+  }).catch(error => {
+    console.error(`Error editing persistent message: ${error}`);
   });
-});
+} else {
+  console.error(`Error editing persistent message: persistentMessage is null`);
+}
 
-console.log('TOKEN : ' + TOKEN);
+// Login the client to Discord
+client.login(TOKEN)
+  .then(() => console.log('Discord bot logged in'))
+  .catch((error) => {
+    console.error(`Error logging in the Discord bot: ${error}`);
+    process.exit(1);
+  });
 
-client.login(TOKEN);
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+// Start the Express server
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Express server started on port ${process.env.PORT || 3000}`);
 });
