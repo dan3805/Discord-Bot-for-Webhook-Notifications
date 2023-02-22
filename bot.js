bba50d4339c014bf3f991516da0ff12a694f253f
DoCtEuR3805 | FRENCH-QC <35934387+dan3805@users.noreply.github.com>
2023-02-21 19:34:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const express = require('express');
 const bodyParser = require('body-parser');
